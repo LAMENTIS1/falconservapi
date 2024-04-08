2622c46731638f49cb97775ac1bea71d4371587a
LAMENTIS1 <124012543+LAMENTIS1@users.noreply.github.com>
2024-04-08 18:57:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,6 @@ def transcribe_audio():
 
                 # Load the model for transcription
                 model_folder_path = "faster-whisper-tiny"
-"
                 model = WhisperModel(model_folder_path, device="cpu", compute_type="int8")
 
                 # Transcribe audio
@@ -55,25 +54,26 @@ def allowed_audio_format(filename):
     allowed_extensions = {'wav', 'flac', 'ogg'}
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in allowed_extensions
 
-if __name__ == '__main__':
-    # Initialize GVP Bot
-    os.environ['API_KEY'] = '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'
-    model_id = 'tiiuae/falcon-7b-instruct'
+os.environ['API_KEY'] = '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'
+model_id = 'tiiuae/falcon-7b-instruct'
 
-    falcon_llm = HuggingFaceHub(huggingfacehub_api_token=os.environ['API_KEY'],
+falcon_llm = HuggingFaceHub(huggingfacehub_api_token=os.environ['API_KEY'],
                                 repo_id=model_id,
                                 model_kwargs={"temperature": 0.8, "max_new_tokens": 2000})
 
-    template = """
+template = """
     Hello! I am GVP Bot, your robotic AI assistant ready to provide helpful answers and solutions to your queries.
     User: {question}
     """
 
-    prompt = PromptTemplate(template=template, input_variables=['question'])
-    falcon_chain = LLMChain(llm=falcon_llm, prompt=prompt, verbose=True)
+prompt = PromptTemplate(template=template, input_variables=['question'])
+falcon_chain = LLMChain(llm=falcon_llm, prompt=prompt, verbose=True)
 
     #print("GVP Bot: Hello! How can I assist you today?")
 
+if __name__ == '__main__':
+    # Initialize GVP Bot
+
+
     # Start Flask app
-    app.run(debug=True)
- # Bind to all interfaces for cloud deployment
+    app.run()  # Bind to all interfaces for cloud deployment
