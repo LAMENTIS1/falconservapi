9cc974e50e468625ab87c1cada9cc53b82491263
LAMENTIS1 <124012543+LAMENTIS1@users.noreply.github.com>
2024-04-09 16:50:49 +0530

    Update app.py load model path github

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def transcribe_audio():
                 audio_data = audio_file.read()
 
                 # Load the model for transcription
-                model_folder_path = "https://raw.githubusercontent.com/LAMENTIS1/tree/main/faster-whisper-tiny"
+                model_folder_path = "/LAMENTIS1/tree/main/faster-whisper-tiny"
                 model = WhisperModel(model_folder_path, device="cpu", compute_type="int8")
 
                 # Transcribe audio
